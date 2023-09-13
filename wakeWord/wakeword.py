88db8ab7fd840bdd39a70a9e7cfebf3550b6a6e1
FairyAmber <56006947+FairyAmber@users.noreply.github.com>
2023-09-12 21:57:15 -0400

    upload hot word model

--- wakeWord/wakeword.py
+++ wakeWord/wakeword.py
@@ -1,11 +1,12 @@
 import pvporcupine
 import pyaudio
+import struct
 
 #API KEY for hot word picovoice
 PICOVOICE_API_KEY = "KNIrwq9lmXqHfNxhvDTDPsFQ5hTYOAmGdK0PAQqaFtPunAsbXHO2Vg=="
 porcupine = pvporcupine.create(
     access_key= PICOVOICE_API_KEY,
-    keyword_paths=[],
+    keyword_paths=[".\Hey-Mickey_en_windows_v2_2_0\Hey-Mickey_en_windows_v2_2_0.ppn"],
 
 )
 
@@ -15,7 +16,13 @@ stream = myaudio.open(
     rate= porcupine.sample_rate,
     channels=1,
     format=myaudio.paInt16,
+    input=True,
     frames_per_buffer=porcupine.frame_length
 )
 
-audio_obj = stream.read()
+audio_obj = stream.read(porcupine.frame_length, exception_on_overflow=False)
+audio_obj_unpacked = struct.unpack_from("h" * porcupine.frame_length, audio_obj)
+
+keyword_idx = porcupine.process(audio_obj_unpacked)
+
+    
