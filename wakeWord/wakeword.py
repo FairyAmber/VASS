8fec34773413946322be5aea18982d4a03cdc352
FairyAmber <quanquanjiang860@gmail.com>
2023-09-12 22:12:03 -0400

    keyword change test

--- wakeWord/wakeword.py
+++ wakeWord/wakeword.py
@@ -5,9 +5,9 @@ import struct
 #API KEY for hot word picovoice
 PICOVOICE_API_KEY = "KNIrwq9lmXqHfNxhvDTDPsFQ5hTYOAmGdK0PAQqaFtPunAsbXHO2Vg=="
 porcupine = pvporcupine.create(
-    access_key= PICOVOICE_API_KEY,
-    keyword_paths=[".\Hey-Mickey_en_windows_v2_2_0\Hey-Mickey_en_windows_v2_2_0.ppn"],
-
+    access_key = PICOVOICE_API_KEY,
+    #keyword_paths = ['.\Hey-Mickey_en_windows_v2_2_0\Hey-Mickey_en_windows_v2_2_0.ppn'],
+    keywords=['bumblebee', 'picovoice']
 )
 
 myaudio = pyaudio.PyAudio()
@@ -15,15 +15,16 @@ stream = myaudio.open(
     input_device_index=0,
     rate= porcupine.sample_rate,
     channels=1,
-    format=myaudio.paInt16,
+    format=pyaudio.paInt16,
     input=True,
     frames_per_buffer=porcupine.frame_length
 )
 
-audio_obj = stream.read(porcupine.frame_length, exception_on_overflow=False)
-audio_obj_unpacked = struct.unpack_from("h" * porcupine.frame_length, audio_obj)
+while True:
+    audio_obj = stream.read(porcupine.frame_length, exception_on_overflow = False)
+    audio_obj_unpacked = struct.unpack_from("h" * porcupine.frame_length, audio_obj)
 
-keyword_idx = porcupine.process(audio_obj_unpacked)
+    keyword_idx = porcupine.process(audio_obj_unpacked)
 
-if keyword_idx >= 0:
-    print("I heard it.")    
+    if keyword_idx >= 0:
+        print("I heard it.")    
